42a224c04ab5969b5a9ab3e75c9edca6998cbff0
Preetham PM <p14preetham@gmail.com>
2025-08-10 18:49:06 +0530

    compose file and changes in server.js

--- server.js
+++ server.js
@@ -4,15 +4,18 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
+const replicaName = process.env.APP_NAME
+
 // Serve static files (CSS, images, etc.) from "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve the HTML file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'elephants.html'));
+    console.log(`Request served by ${replicaName}`);
 });
 
 // Start server
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`${replicaName} running at http://localhost:${PORT}`);
 });
